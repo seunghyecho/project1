a65b7997bd3eea06564232cb8d5d17609da1485c
seunghyecho <tmdgp523@naver.com>
2020-12-28 21:29:25 +0900

    html css js 정리

--- src/main/intro/intro.js
+++ src/main/intro/intro.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
     console.log('main intro');
 
-    $(".myScroll").myScroll({
-        btns: "#navi>li",
-        speed: 1000,
-        base: 0,
-        active: "on",
-    });
+    // $(".myScroll").myScroll({
+    //     btns: "#navi>li",
+    //     speed: 800,
+    //     base: 0,
+    //     active: "on",
+    // });
 
      // CONTENT1 CLICK BUTTON
      var buttons = $("#content1").find("a");
@@ -91,45 +91,45 @@ $(document).ready(function(){
     });
 
     // 스크롤시 컨텐츠 마다 .on
-    $(window).on('scroll', function(){
-        var scroll = $(this).scrollTop();
-        var pos0 = $('.myScroll').eq(0).offset().top; //content1
-        var pos1 = $('.myScroll').eq(1).offset().top; //tab
-        var pos2 = $('.myScroll').eq(2).offset().top; //content2
-        var pos3 = $('.myScroll').eq(3).offset().top; //content3
-        var pos4 = $('.myScroll').eq(4).offset().top; //content4
-        // console.log("scroll:"+scroll);
-        // console.log("pos0:"+pos0);
-        // console.log("pos1:"+pos1);
-        // console.log("pos2:"+pos2);
-        // console.log("pos3:"+pos3);
-        // console.log("pos4:"+pos4);
+//     $(window).on('scroll', function(){
+//         var scroll = $(this).scrollTop();
+//         var pos0 = $('.myScroll').eq(0).offset().top; //content1
+//         var pos1 = $('.myScroll').eq(1).offset().top; //tab
+//         var pos2 = $('.myScroll').eq(2).offset().top; //content2
+//         var pos3 = $('.myScroll').eq(3).offset().top; //content3
+//         var pos4 = $('.myScroll').eq(4).offset().top; //content4
+//         console.log("scroll:"+scroll);
+//         console.log("pos0:"+pos0);
+//         console.log("pos1:"+pos1);
+//         console.log("pos2:"+pos2);
+//         console.log("pos3:"+pos3);
+//         console.log("pos4:"+pos4);
 
 
-        // if(scroll < 300){
-        //     $('header').removeClass('on');
-        //     $('#navi').hide();
-        // }else if(scroll >= (pos0 - 400) && scroll < pos1 ){
-        //     $('header').addClass('on');
-        //     $('#navi').show();
-        //     $('#content1').addClass('on');
-        // }else if(scroll >= (pos1 - 200) && scroll < pos2 ){
-        //     $('#tab').addClass('on');
-        // }else if(scroll >= (pos2 - 200) && scroll < pos3){
-        //     $('#content2').addClass('on');
-        // }else if(scroll >= (pos3 - 200) && scroll < pos4){
-        //     $('#content3').addClass('on');
-        // }else if(scroll >= (pos4 - 200) ){
-        //     $('#content4').addClass('on');
-        // }
+//         if(scroll < 300){
+//             $('header').removeClass('on');
+//             $('#navi').hide();
+//         }else if(scroll >= (pos0 - 400) && scroll < pos1 ){
+//             $('header').addClass('on');
+//             $('#navi').show();
+//             $('#content1').addClass('on');
+//         }else if(scroll >= (pos1 - 200) && scroll < pos2 ){
+//             $('#tab').addClass('on');
+//         }else if(scroll >= (pos2 - 200) && scroll < pos3){
+//             $('#content2').addClass('on');
+//         }else if(scroll >= (pos3 - 200) && scroll < pos4){
+//             $('#content3').addClass('on');
+//         }else if(scroll >= (pos4 - 200) ){
+//             $('#content4').addClass('on');
+//         }
 
-    });
+//     });
     
-        // $('header').removeClass('on');
-        // $('#content1').removeClass('on');
-        // $('#content2').removeClass('on');
-        // $('#content4').removeClass('on');
-        // $('#content3').removeClass('on');
+//         $('header').removeClass('on');
+//         $('#content1').removeClass('on');
+//         $('#content2').removeClass('on');
+//         $('#content4').removeClass('on');
+//         $('#content3').removeClass('on');
 
         
 
